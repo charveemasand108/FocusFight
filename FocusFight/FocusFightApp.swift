f41376125e011aa63af12b2a869f30905a4f1428
Charvee Masand <charvee.masand@gmail.com>
2026-04-26 12:11:58 +0530

    Initial Commit

---
+++ FocusFight/FocusFightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFightApp.swift
+//  FocusFight
+//
+//  Created by Charvee Masand on 26/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusFightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
